de83542d344b3957eb746dfe21dcec57024d5e13
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-14 22:08:48 -0500

    funciona pero sin dejar nada en la carpeta

--- index.js
+++ index.js
@@ -1,40 +1,79 @@
 const fs = require('fs')
-const readline = require('readline');
-
+const lineReader = require('line-reader');
 const PATH = './file2parse/logins.csv';
-// fs.readFile('./file2parse/logins.csv', 'utf8' , (err, data) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-//   console.log(data[3]);
-//   // for()
-// })
+const MAX_CONTENT = 1000;
+const os = require("os");
 
-const readInterface = readline.createInterface({
-  input: fs.createReadStream(PATH),
-  output: null,
-  console: false
-});
+async function parseToBitwarden() {
+  const [ header, ...arrayJSON] = await parseToJSON();
+  const contentMinusHeader = MAX_CONTENT - header.len;
+  console.log('contentMinusHeader', contentMinusHeader);
+  // console.log('arrayJSON', arrayJSON)
+  const arrayOfSections = sectionJSON(arrayJSON, contentMinusHeader)
+  parseArrayToFile(arrayOfSections);
+}
 
-readInterface.on('line', function(line) {
-  console.log('line', line);
-  console.log('line', line.length);
-});
+function parseArrayToFile(array) {
+  let fileNum = 1;
+  for(let arr of array) {
 
-function CSVParsed() {
-  return new Promise((resolved, reject) => {
-    try {
-      readInterface.on('line', function(line) {
-        if (a < 6) {
-          console.log('line', line);
-          console.log('line', line.length);
-        }
-      });
-
-    } catch (error) {
-      return reject(error);
+    const logger = fs.createWriteStream(`${fileNum}.txt`, {
+      flags: 'a' // 'a' means appending (old data will be preserved)
+    })
+    for(let line of arr) {
+      logger.write(line.content + os.EOL) // append string to your file
     }
+    logger.end();
+    fileNum = fileNum + 1;
+  }
+}
+
+
+
+function parseToJSON() {
+  return new Promise((resolve, reject) => {
+    let arrayParsed = [];
+    lineReader.eachLine(PATH, function(line, last) {
+      const obj = {
+        content: line,
+        len: line.length
+      };
+      arrayParsed = [...arrayParsed, obj];
+      if (last) {
+        return resolve(arrayParsed)
+      }
+    });
   })
 }
 
+
+
+function sectionJSON(array, limit, iterador = 0, contentResult = []) {
+  if (contentResult.length == 0) {
+    contentResult.push([]);
+  }
+  const lastActualItem = contentResult[contentResult.length - 1];
+  const sumLastActualItem = sumArrayByKey(lastActualItem, 'len');
+  const itemIterador = array[iterador];
+
+  if(sumLastActualItem + itemIterador.len <= limit) {
+    contentResult[contentResult.length - 1].push(itemIterador)
+  } else {
+    contentResult = [...contentResult, [itemIterador]]
+  }
+
+  if (array.length - 1 > iterador) {
+    return sectionJSON(array, limit, iterador + 1, contentResult)
+  } else {
+    return contentResult
+  }
+}
+
+function sumArrayByKey(array, key) {
+  return [...array].reduce((acc,item)=> {
+    return acc + item[key]
+  }, 0);
+}
+
+
+parseToBitwarden();
